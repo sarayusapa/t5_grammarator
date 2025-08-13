a310e413e3229734683d819834522fb6295501ee
Sarayu Anantharaman <sarayusapa@gmail.com>
2025-08-13 20:20:58 +0530

    Update QLoRA.py

--- QLoRA.py
+++ QLoRA.py
@@ -69,8 +69,8 @@ def main() -> None:
 
 
     #small batch for testing, comment out later
-    train_dataset = train_dataset.select(range(1500000))  # first 100000 samples
-    eval_dataset = eval_dataset.select(range(10000))    # first 10000 samples
+    train_dataset = train_dataset.select(range(500000))  # first 100000 samples
+    eval_dataset = eval_dataset.select(range(5000))    # first 10000 samples
 
 
     # Infer source/target fields
@@ -211,6 +211,7 @@ def main() -> None:
 if __name__ == "__main__":
 
     main()
+
 
 
 
