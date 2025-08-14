bda72cf6eeadda70e1703f959e8c8df8837feb85
Sarayu Anantharaman <sarayusapa@gmail.com>
2025-08-14 16:18:35 +0530

    Update pushmodel.py

--- pushmodel.py
+++ pushmodel.py
@@ -1,5 +1,6 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from peft import PeftModel, PeftConfig, get_peft_model
+import torch
 
 base_model = AutoModelForSeq2SeqLM.from_pretrained("t5-large", torch_dtype=torch.float16)
 model = PeftModel.from_pretrained(base_model, "./qlora-flan-t5-base-lang8")
