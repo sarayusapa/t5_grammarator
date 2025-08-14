2da1311d2968a49f1fb4d0306894c5baa9642980
Sarayu Anantharaman <sarayusapa@gmail.com>
2025-08-14 16:04:46 +0530

    Updated

--- pushmodel.py
+++ pushmodel.py
@@ -1,12 +1,12 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 # Path to your saved model folder
-model_path = "/full/path/to/your/model"
+model_path = "./T5-grammarator"
 
 # Load model & tokenizer
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # Push to Hugging Face Hub
-model.push_to_hub("sarayusapa/t5_lora_peft_output")
-tokenizer.push_to_hub("sarayusapa/t5_lora_peft_output")
+model.push_to_hub("sarayusapa/T5_large_QLoRA")
+tokenizer.push_to_hub("sarayusapa/T5_large_QLoRA")
